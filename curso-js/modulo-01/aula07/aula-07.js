3660083a329a9ccf0ac197f33514088a3c42600c
nely de faria <nelly.nf54@gmail.com>
2021-09-09 17:32:07 -0300

    Adiciona arquivo aula-07.js

---
+++ curso-js/modulo-01/aula07/aula-07.js
@@ -0,0 +1,9 @@
+var nome = "Bonieky";
+console.log(window.nome);
+
+if (nome === "Bonieky") {
+  let idade = 90;
+  console.log(idade);
+}
+
+const h1_alterado = document.querySelector("#titulo").innerHTML = "Variáveis";
