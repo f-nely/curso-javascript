fa1a34fc59c6c95995d1c89b8403a336ba8ad4cc
nely de faria <nelly.nf54@gmail.com>
2021-09-09 17:27:48 -0300

    Adiciona arquivo aula-04.js

---
+++ curso-js/modulo-01/aula04/aula-04.js
@@ -0,0 +1,6 @@
+var first_name = "nelynely";
+var age = 81;
+
+console.log("My name is " + first_name + " and I am " + age + " years old.");
+
+console.log(`My name is ${first_name} and I'm ${age} years old.`);
