747e7d2db0515aa5629cd02697c3ed345c573b54
nely de faria <nelly.nf54@gmail.com>
2021-09-10 13:06:04 -0300

    Adiciona arquivo aula-02.js

---
+++ curso-js/modulo-02/aula02/aula-02.js
@@ -0,0 +1,4 @@
+document.getElementById("btn");
+document.getElementsByClassName("list")[0].innerHTML = "item alterado!";
+document.getElementsByTagName("button");
+document.getElementsByName("email");
