6e469770bcd83d3ae49fcc1823b9c23dff33e27b
nely de faria <nelly.nf54@gmail.com>
2021-09-10 16:09:18 -0300

    Criando arquivo aula-06.js

---
+++ curso-js/modulo-02/aula06/aula-06.js
@@ -0,0 +1,10 @@
+var nome = "Bonieky";
+var idade = 90;
+var salvo = true;
+var cidade;
+
+// typeof 
+console.log(typeof(nome));
+console.log(typeof(idade));
+console.log(typeof(salvo));
+console.log(typeof(cidade));
