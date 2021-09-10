e927850f6eb41799d86f84548dfc60b4b3a2ad4d
nely de faria <nelly.nf54@gmail.com>
2021-09-10 13:22:23 -0300

    Adiciona arquivo aula-03.js

---
+++ curso-js/modulo-02/aula03/aula-03.js
@@ -0,0 +1,8 @@
+document.querySelector(".example").innerHTML = "f-nely";
+document.querySelector("button").innerHTML = "Enviar";
+const email = document.querySelector("#email");
+console.log(email);
+const div = document.querySelectorAll("div");
+console.log(div);
+const list = document.querySelectorAll(".list");
+console.log(list);
