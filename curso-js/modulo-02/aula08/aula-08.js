47e209c650c22b90f04abe92bb798f2924857719
nely de faria <nelly.nf54@gmail.com>
2021-09-10 16:47:01 -0300

    Criando arquivo aula-08.js

---
+++ curso-js/modulo-02/aula08/aula-08.js
@@ -0,0 +1,15 @@
+function alterar(titulo) {
+  document.getElementById("titulo").innerHTML = titulo;
+  document.getElementById("nome").value = titulo;
+}
+
+alterar("Titulo de exemplo!");
+
+function sum(num1, num2) {
+  let total = num1 + num2;
+
+  document.getElementById("nome").value = total;
+}
+
+sum(10, 1);
+
