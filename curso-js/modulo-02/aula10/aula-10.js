974d794bc4bc85155db8c5f0a039465ca6f0e73e
nely de faria <nelly.nf54@gmail.com>
2021-09-11 11:22:22 -0300

    Criando arquivo aula-10.js

---
+++ curso-js/modulo-02/aula10/aula-10.js
@@ -0,0 +1,6 @@
+console.log(document.querySelector('.text').offsetHeight);
+console.log(document.querySelector('.text').offsetWidth);
+console.log(document.querySelector('.text').clientWidth);
+console.log(document.querySelector('.text').clientHeight);
+console.log(document.querySelector('.text').scrollWidth);
+console.log(document.querySelector('.text').scrollHeight);
