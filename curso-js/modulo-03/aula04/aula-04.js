b814d4a1941a838784869420e6fbf71ccaa7dc64
nely de faria <nelly.nf54@gmail.com>
2021-09-12 13:05:08 -0300

    Criando arquivo aula-04.js

---
+++ curso-js/modulo-03/aula04/aula-04.js
@@ -0,0 +1,12 @@
+let cars = ['Palio', 'Uno', 'Corolla', 'S10'];
+
+console.log(cars[0]);
+
+let fruits = ['Apple', 'Banana'];
+
+fruits.push('Orange');
+fruits.unshift('Strawberry');
+
+for(let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
