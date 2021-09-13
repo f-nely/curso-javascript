da1e074966bc3d4abd48cb88887facff514bc6a7
nely de faria <nelly.nf54@gmail.com>
2021-09-13 15:22:26 -0300

    Criando arquivo aula-01.js

---
+++ curso-js/modulo-04/aula01/aula-01.js
@@ -0,0 +1,8 @@
+let complete_name = 'Bonieky Lacerda Leal';
+
+console.log(complete_name.length);
+console.log(complete_name.indexOf('Lacerda'));
+
+for(let i in complete_name) {
+  console.log(complete_name[i])
+}
