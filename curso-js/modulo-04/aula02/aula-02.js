43b172657c0f1264a1fe85292cdcf06b6c317aea
nely de faria <nelly.nf54@gmail.com>
2021-09-13 15:48:41 -0300

    Criando arquivo aula-02.js

---
+++ curso-js/modulo-04/aula02/aula-02.js
@@ -0,0 +1,18 @@
+let complete_name = 'Bonieky Lacerda Leal';
+
+//let resul = complete_name.slice(0, 10);
+//let resul = complete_name.slice(15);
+//let resul = complete_name.slice(-4);
+//console.log(resul);
+
+// Exemplo para extrair 'Laranja' e 'Limao' do array frutas
+let frutas = ['Banana', 'Laranja', 'Limão', 'Maçã', 'Manga'];
+let citricos = frutas.slice(1, 3);
+// citricos contem ['Laranja','Limao']
+//console.log(citricos);
+
+//let resul = complete_name.substring(0, 10);
+//console.log(resul);
+
+let resul = complete_name.substr(0, 7);
+console.log(resul);
