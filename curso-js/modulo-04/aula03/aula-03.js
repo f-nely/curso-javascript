3ab6f0076cd78315a2cb568fb68f5f9728bc452d
nely de faria <nelly.nf54@gmail.com>
2021-09-13 16:19:18 -0300

    Criando arquivo aula-03.js

---
+++ curso-js/modulo-04/aula03/aula-03.js
@@ -0,0 +1,25 @@
+//let complete_name = 'He likes cars';
+
+//let resul = complete_name.replace('cars', 'motorcycles');
+//console.log(resul);
+
+//let resul = complete_name.toUpperCase();
+//console.log(resul);
+//let resul = complete_name.toLowerCase();
+//console.log(resul);
+//let resul = complete_name.concat('. and you?');
+//console.log(resul);
+
+//let nome = '   nelynely   ';
+//let result = nome.trim().length;
+//console.log('result: ' ,result);
+//console.log(result);
+
+let nome = 'Steve Jobs';
+let cars = 'S10, Jetta, Amarok';
+//let result = nome.charAt(0);
+//console.log(result);
+let result = nome.split(' ');
+console.log(result);
+let result2 = cars.split(',');
+console.log(result2);
