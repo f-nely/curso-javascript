d6bc715b373f6d7576d1f68a765e8eedef874324
nely de faria <nelly.nf54@gmail.com>
2021-09-14 09:14:21 -0300

    Criando arquivo aula-04.js

---
+++ curso-js/modulo-04/aula04/aula-04.js
@@ -0,0 +1,16 @@
+//let number = 10;
+//console.log(typeof(number));
+//let result = number.toString();
+//console.log(typeof(result));
+
+//let number = 10.65478;
+//let result = 'R$ ' + number.toFixed(2);
+//console.log(result);
+
+//let n = '25';
+//let result = parseInt(n) + 5;
+//console.log(result);
+
+let n = '25.90';
+let result = parseFloat(n) + 5;
+console.log(result);
