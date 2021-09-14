39f23594d3f11e28d3446cca6fe6e1744a696c73
nely de faria <nelly.nf54@gmail.com>
2021-09-14 09:35:39 -0300

    Criando arquivo aula-05.js

---
+++ curso-js/modulo-04/aula05/aula-05.js
@@ -0,0 +1,10 @@
+let cars = ['Jetta', 'S10', 'Renegade', 'Amarok', 'Jeep Compass'];
+//let result = cars.toString();
+//let result = cars.join('-');
+//let result = cars.indexOf('S10');
+//cars.pop(); - remove o último elemento
+//cars.shift(); - remove o primeiro elemento
+cars.push('Cronos');
+cars.unshift('Logan');
+let result = cars;
+console.log(result);
