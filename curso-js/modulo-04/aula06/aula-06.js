d643b0a39751719dd2a04bdc68ee4a68887f7286
nely de faria <nelly.nf54@gmail.com>
2021-09-14 09:53:39 -0300

    Criando arquivo aula-06.js

---
+++ curso-js/modulo-04/aula06/aula-06.js
@@ -0,0 +1,10 @@
+let cars = ['Jetta', 'S10', 'Renegade', 'Amarok', 'Jeep Compass'];
+//let motos = ['Honda CB', 'Ducati Diavel', 'Suzuki GSX-R100']
+
+//cars.splice(4, 1);
+//let result = cars.concat(motos);
+
+cars.sort();
+cars.reverse();
+let result = cars;
+console.log(result);
