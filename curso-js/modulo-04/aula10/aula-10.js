de5762a6be5d8510056c0892be57a123ba7c8b8a
nely de faria <nelly.nf54@gmail.com>
2021-09-15 14:12:17 -0300

    Criando arquivo aula-10.js

---
+++ curso-js/modulo-04/aula10/aula-10.js
@@ -0,0 +1,11 @@
+//let d = new Date();
+//let new_value = d.getFullYear();
+//let new_value = d.getMonth(); // mês começa do 0 em js
+//let new_value = d.getDate();
+//let new_value = d.getHours();
+//let new_value = d.getMinutes();
+//let new_value = d.getSeconds();
+//let new_value = d.getMilliseconds();
+//let new_value = d.getTime();
+let new_value = Date.now();
+console.log(new_value);
