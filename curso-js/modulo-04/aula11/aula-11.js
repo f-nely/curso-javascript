930b90f2c8fe5861c9abd893b1c8952871168ac4
nely de faria <nelly.nf54@gmail.com>
2021-09-15 14:26:50 -0300

    Criando arquivo aula-11.js

---
+++ curso-js/modulo-04/aula11/aula-11.js
@@ -0,0 +1,8 @@
+let d = new Date();
+//d.setFullYear(2022);
+//d.setMonth(11); // dezembro
+//d.setDate(d.getDate()+5);
+//d.setDate(d.getDate()+9);
+d.setHours(d.getHours()+3);
+let new_value = d;
+console.log(new_value);
