bfa7ef9127f94bc7e7340785f68c7a379c090447
nely de faria <nelly.nf54@gmail.com>
2021-09-15 13:49:42 -0300

    Criando arquivo aula-09.js

---
+++ curso-js/modulo-04/aula09/aula-09.js
@@ -0,0 +1,8 @@
+//let d = new Date(2021, 0, 1, 13, 39, 12);
+//console.log(d);
+//console.log(d.toDateString());
+//console.log(d.toUTCString());
+//let d = new Date('2020-01-01 15:42:17');
+//let d = new Date(2022, 2);
+let d = new Date(0);
+console.log(d.toUTCString());
