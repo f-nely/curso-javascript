892c4198c4b8212ee06fbb267fd3a69da7d4a76c
nely de faria <nelly.nf54@gmail.com>
2021-09-17 15:23:43 -0300

    Criando arquivo aula-13.js

---
+++ curso-js/modulo-04/aula13/aula-13.js
@@ -0,0 +1,21 @@
+let timer;
+
+function start() {
+  timer = setInterval(showTime, 1000);
+}
+
+function pause() {
+  clearInterval(timer);
+}
+
+function showTime() {
+  let d = new Date();
+  let h = d.getHours();
+  let m = d.getMinutes();
+  let s = d.getSeconds();
+
+  let txt = `${h} : ${m} : ${s}`;
+
+  document.querySelector('.demo').innerHTML = txt;
+}
+
