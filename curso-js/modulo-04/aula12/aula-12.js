428d0770cd2fde5b8952a2f50a01b42a6c8a61e1
nely de faria <nelly.nf54@gmail.com>
2021-09-17 14:58:05 -0300

    Criando arquivo aula-12.js

---
+++ curso-js/modulo-04/aula12/aula-12.js
@@ -0,0 +1,9 @@
+//let new_value = Math.PI;
+//let new_value = Math.round(3.67);
+//let new_value = Math.floor(3.67);
+//let new_value = Math.ceil(3.67);
+//let new_value = Math.abs(-9.65);
+//let new_value = Math.min(10, 75, 20, 11, 3, 7);
+//let new_value = Math.max(10, 75, 20, 11, 3, 7);
+let new_value = Math.floor(Math.random() * 100);
+console.log(new_value);
