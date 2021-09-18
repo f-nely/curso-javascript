12f8e72ce9ed6ac350d9f45dc261450b57f5d109
nely de faria <nelly.nf54@gmail.com>
2021-09-18 18:58:37 -0300

    Criando arquiov aula-19.js

---
+++ curso-js/modulo-04/aula19/aula-19.js
@@ -0,0 +1,10 @@
+/* const sum = (n1, n2) => {
+  return n1 + n2;
+} */
+
+const sum = (n1, n2) => n1 + n2;
+console.log(sum(10, 5));
+
+//const qtLetrasNome = (nome) => nome.length;
+const qtLetrasNome = (nome) => nome.length;
+console.log(qtLetrasNome('Taylor'));
