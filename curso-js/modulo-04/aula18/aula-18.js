2292cb65e0921b55d5e312ee54979fa9251a289e
nely de faria <nelly.nf54@gmail.com>
2021-09-18 18:44:51 -0300

    Criando arquivo aula-18.js

---
+++ curso-js/modulo-04/aula18/aula-18.js
@@ -0,0 +1,10 @@
+//let info = ['Taylor Otwell', 'Taylor', 'Otwell', '@taylorotwell'];
+
+//let [completeName, first_name, last_name, instagram] = info;
+//let [completeName, , , instagram] = info;
+
+//console.log(completeName, first_name, last_name, instagram);
+//console.log(completeName, instagram);
+
+let [first_name, last_name] = ['Taylor', 'Otwell'];
+console.log(first_name, last_name);
