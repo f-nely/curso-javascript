9b6ef51ef2ea04604fd4cb4fc8d732d798a26ef0
nely de faria <nelly.nf54@gmail.com>
2021-09-18 19:44:43 -0300

    Criandoa arquivo aula-23.js

---
+++ curso-js/modulo-04/aula23/aula-23.js
@@ -0,0 +1,16 @@
+let cars = ['Jetta', 'S10', 'Jeep Compass', 'Renegade', 'Logan', 'Renault KWID'];
+
+console.log(Object.keys(cars));
+console.log(Object.values(cars));
+console.log(Object.entries(cars));
+
+let people = {
+  first_name: 'Taylor',
+  last_name: 'Otwell',
+  age: 32,
+}
+
+console.log(Object.keys(people));
+console.log(Object.values(people));
+console.log(Object.entries(people));
+
