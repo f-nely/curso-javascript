8aa2cb9ea8d124156946c6c09add5bb3aa4f10d0
nely de faria <nelly.nf54@gmail.com>
2021-09-22 14:34:55 -0300

    Criando arquivo aula-01.js

---
+++ curso-js/modulo-05/aula01/aula-01.js
@@ -0,0 +1,16 @@
+let pessoa = {
+  nome: 'Taylor', 
+  idade: 31,
+  caracteristica: ['solicito', 'pontual'],
+  estetica: {
+    altura: 1.8,
+    peso: 80
+  }
+}
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.caracteristica[0]);
+console.log(pessoa.caracteristica[1]);
+console.log(pessoa.estetica.altura);
+console.log(pessoa.estetica.peso);
